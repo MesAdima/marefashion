9b9abddf173b6554da6780c4764d92316fb07919
Adima Mesa <mesadima@gmail.com>
2015-02-23 15:08:57 +0300

    very minor commit. Usipull

--- application/modules/shop/views/shopview.php
+++ application/modules/shop/views/shopview.php
@@ -15,7 +15,7 @@
 
 <?php foreach ($this->cart->contents() as $items): ?>
 
-	<?php echo form_hidden($i.'[rowid]', $items['rowid']); ?>
+	<?php echo form_hidden('rowid[]', $items['rowid']); ?>
 
 	<tr>
 	  <td><?php echo form_input(array('name' => $i.'[qty]', 'value' => $items['qty'], 'maxlength' => '3', 'size' => '5')); ?></td>
